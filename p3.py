56ed10be49d82967f6d8b01ce575abaf8cb791bc
finnegantdewitt <finn@finnegandewitt.com>
2021-08-20 10:43:00 -0700

    part 3

---
+++ p3.py
@@ -0,0 +1,24 @@
+# https://www.youtube.com/watch?v=tMrbN67U9d4&list=PLQVvvaa0QuDcjD5BAw2DxE6OF2tius3V3&index=3
+
+import numpy as np
+
+inputs = [1, 2, 3, 2.5] 
+
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
+
+biases = [2, 3, 0.5]
+
+output = np.dot(weights, inputs) + biases
+print(output)
+
+# layer_outputs = []
+# for neuron_weights, neuron_bias in zip(weights, biases):
+#     neuron_output = 0
+#     for n_input, weight in zip(inputs, neuron_weights):
+#         neuron_output += n_input*weight
+#     neuron_output += neuron_bias
+#     layer_outputs.append(neuron_output)
+
+# print(layer_outputs)
